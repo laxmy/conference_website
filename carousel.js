03eef77cd15e99d18f6d755f693ca8aeca772375
laxmy <laxmysubu@gmail.com>
2017-06-16 12:49:38 -0500

    Changes excluding carousel and register form alignment

---
+++ carousel.js
@@ -0,0 +1,35 @@
+var currentIndex = 0;
+var items = $('.carousel-slide');
+var itemAmt = items.length;
+
+function cycleItems() {
+  var item = $('.carousel-slide').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
+
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 1000);
+
+$('#next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('#prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
+});
